9e9949fb60890c38ea3975fb77289bf34657a52a
pavanepour-k <pavanepour@outlook.com>
2025-06-24 08:59:35 +0900

    Fix module naming

--- rustlib/src/lib.rs
+++ rustlib/src/lib.rs
@@ -5,19 +5,19 @@ mod hashing;
 mod regex_utils;
 mod file_ops;
 mod filter;
-mod random;
+mod random_utils;
 mod compress;
 mod net_utils;
 mod data_proc;
 
 /// Main Python module initializer
 #[pymodule]
-pub fn rustlib(py: Python, m: &PyModule) -> PyResult<()> {
+pub fn rustlib(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapping(wrap_pymodule!(hashing, m)?)?;
     m.add_wrapping(wrap_pymodule!(regex_utils, m)?)?;
     m.add_wrapping(wrap_pymodule!(file_ops, m)?)?;
     m.add_wrapping(wrap_pymodule!(filter, m)?)?;
-    m.add_wrapping(wrap_pymodule!(random, m)?)?;
+    m.add_wrapping(wrap_pymodule!(random_utils, m)?)?;
     m.add_wrapping(wrap_pymodule!(compress, m)?)?;
     m.add_wrapping(wrap_pymodule!(net_utils, m)?)?;
     m.add_wrapping(wrap_pymodule!(data_proc, m)?)?;
