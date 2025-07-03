618cc65e27abc83dba788a25ec4c0360169e8f0a
pavanepour-k <pavanepour@outlook.com>
2025-07-03 17:51:25 +0900

    Integration Corrected

--- rust_src/types/mod.rs
+++ rust_src/types/mod.rs
@@ -1,6 +1,6 @@
 pub mod models;
 
-use crate::core::Route;
+use crate::core::routing::APIRoute;
 use crate::params::ValidationResult as RustValidationResult;
 use pyo3::prelude::*;
 use serde_json::Value;
@@ -17,44 +17,37 @@ pub struct FastApiRoute {
     pub name: Option<String>,
     #[pyo3(get)]
     pub path_format: String,
-    pub(crate) inner: Route,
+    pub(crate) inner: APIRoute,
 }
 
 #[pymethods]
 impl FastApiRoute {
     #[new]
     pub fn new(path: String, methods: Vec<String>, name: Option<String>) -> PyResult<Self> {
-        let route = Route::new(&path, methods.clone(), name.clone())
+        let mut route = APIRoute::new(path.clone(), methods.clone());
+        if let Some(n) = name.clone() {
+            route = route.with_name(n);
+        }
+        
+        route.compile()
             .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
 
         Ok(FastApiRoute {
-            path: path.clone(),
+            path,
             methods,
             name,
-            path_format: route.path_format.clone(),
+            path_format: route.path_format.clone().unwrap_or_default(),
             inner: route,
         })
     }
 
     pub fn matches(&self, path: &str, method: &str) -> bool {
-        if !self.methods.iter().any(|m| m == method) {
-            return false;
-        }
-        self.inner.regex.is_match(path)
+        self.inner.matches(path, method).is_some()
     }
 
     pub fn extract_params(&self, path: &str) -> Option<HashMap<String, String>> {
-        if let Some(captures) = self.inner.regex.captures(path) {
-            let mut params = HashMap::new();
-            for (i, param_name) in self.inner.param_names.iter().enumerate() {
-                if let Some(capture) = captures.get(i + 1) {
-                    params.insert(param_name.clone(), capture.as_str().to_string());
-                }
-            }
-            Some(params)
-        } else {
-            None
-        }
+        self.inner.matches(path, &self.methods[0])
+            .map(|params| params.into_iter().collect())
     }
 
     fn __repr__(&self) -> String {
@@ -66,18 +59,14 @@ impl FastApiRoute {
 }
 
 impl FastApiRoute {
-    pub fn from(route: Route) -> Self {
+    pub fn from(route: APIRoute) -> Self {
         FastApiRoute {
             path: route.path.clone(),
-            methods: route.methods.iter().cloned().collect(),
+            methods: route.methods.clone(),
             name: route.name.clone(),
-            path_format: route.path_format.clone(),
+            path_format: route.path_format.clone().unwrap_or_default(),
             inner: route,
         }
-    }
-
-    pub fn to_rust_route(&self) -> Route {
-        self.inner.clone()
     }
 }
 
@@ -162,10 +151,9 @@ impl RequestData {
         query_params: HashMap<String, Vec<String>>,
         body: Option<Vec<u8>>,
     ) -> Self {
-        let content_type = headers
-            .get("content-type")
+        let content_type = headers.get("content-type")
             .map(|ct| ct.split(';').next().unwrap_or(ct).trim().to_lowercase());
-
+        
         Self {
             method,
             path,
@@ -175,57 +163,51 @@ impl RequestData {
             body,
         }
     }
-
+    
     pub fn get_header(&self, name: &str) -> Option<String> {
         self.headers.get(&name.to_lowercase()).cloned()
     }
-
+    
     pub fn get_query_param(&self, name: &str) -> Option<String> {
         self.query_params.get(name)?.first().cloned()
     }
-
+    
     pub fn get_query_params(&self, name: &str) -> Option<Vec<String>> {
         self.query_params.get(name).cloned()
     }
-
+    
     pub fn has_body(&self) -> bool {
         self.body.as_ref().map_or(false, |b| !b.is_empty())
     }
-
+    
     pub fn body_size(&self) -> usize {
         self.body.as_ref().map_or(0, |b| b.len())
     }
-
+    
     pub fn is_json(&self) -> bool {
-        self.content_type
-            .as_ref()
+        self.content_type.as_ref()
             .map_or(false, |ct| ct.starts_with("application/json"))
     }
-
+    
     pub fn is_form_data(&self) -> bool {
-        self.content_type.as_ref().map_or(false, |ct| {
-            ct.starts_with("application/x-www-form-urlencoded")
-        })
+        self.content_type.as_ref()
+            .map_or(false, |ct| ct.starts_with("application/x-www-form-urlencoded"))
     }
-
+    
     pub fn is_multipart(&self) -> bool {
-        self.content_type
-            .as_ref()
+        self.content_type.as_ref()
             .map_or(false, |ct| ct.starts_with("multipart/form-data"))
     }
-
+    
     #[getter]
     pub fn body_bytes(&self) -> Option<Vec<u8>> {
         self.body.clone()
     }
-
+    
     fn __repr__(&self) -> String {
         format!(
             "RequestData(method='{}', path='{}', content_type={:?}, body_size={})",
-            self.method,
-            self.path,
-            self.content_type,
-            self.body_size()
+            self.method, self.path, self.content_type, self.body_size()
         )
     }
 }
